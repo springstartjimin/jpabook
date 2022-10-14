8136728336a6f448012587314c03b0c039249332
Song <misosjm@naver.com>
2022-10-14 22:21:17 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.query.criteria.internal.OrderImpl;
 
 import javax.persistence.*;
 
@@ -10,6 +13,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -25,4 +29,27 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    //비즈니스 로직
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    //조회 로직
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepositoy;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepositoy itemRepositoy;
+
+    /**
+     * 주문
+     * @param memberId
+     * @param itemId
+     * @param count
+     * @return
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepositoy.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     * @param orderId
+     */
+
+    @Transactional
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+//    public List<Order> findOrders(OrderSearch orderSearch){
+//        return orderRepository.findAll(orderSearch);
+//    }
+
+
+}
